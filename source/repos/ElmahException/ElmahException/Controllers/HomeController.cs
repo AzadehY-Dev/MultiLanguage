b5dcb6efddb98521306e88fcd94035701289e99d
CarlaY <47314411+CarlaY-Dev@users.noreply.github.com>
2023-11-23 23:21:24 +0330

    customize error page

---
+++ source/repos/ElmahException/ElmahException/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElmahException.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index(string id = "123")
+        {
+            int num = Convert.ToInt32(id);
+            return View();
+        }
+    }
+}
